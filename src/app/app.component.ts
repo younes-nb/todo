c666cb772cf4214d2683aebadc69251e43e9f297
Younes <younesnikbin@gmail.com>
2022-08-15 10:33:35 +0430

    Add dark mode to header and background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo';
+  isDarkMode = false;
+
+  changeMode(mode: string) {
+    document.body.className = `${mode}-body`;
+    switch (mode) {
+      case "light":
+        this.isDarkMode = false;
+        break;
+      case "dark":
+        this.isDarkMode = true;
+        break;
+    }
+  }
 }
